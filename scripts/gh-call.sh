df202f5f59c6735d1ea6c83afe3995871dfb0b62
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-06-14 14:16:34 +0100

    test

--- scripts/gh-call.sh
+++ scripts/gh-call.sh
@@ -8,14 +8,15 @@ limit=5
 repos=$(gh search repos "org:hmcts" "language:HCL" "module in:name" --limit $limit | awk '{print $1}')
 
 # Initialize the ReadMe.md file with the table header
-echo "| TF Module | Consuming Repo Count |" > README.md
-echo "| --- | --- |" >> README.md
+echo "| TF Module | Consuming Repo Count |" > ../README.md
+echo "| --- | --- |" >> ../README.md
 
 # Loop through the filtered repositories and get their count
 for repo in $repos; do
-    count=$(gh search code "org:hmcts" "language:HCL" $repo --limit $limit  | wc -l)
+    count=$(gh search code "org:hmcts" "language:HCL" $repo --limit 500  | wc -l)
     search_url="https://github.com/search?q=org%3Ahmcts+$repo+language%3AHCL&type=code&l=HCL"
     # echo "$repo is used by $count repos"
-    echo "| $repo | [$count]($search_url) |" >> README.md
+    echo "| $repo | <a href=\"$search_url\" target=\"_blank\">$count</a> |" >> ../README.md
+    
     # sleep 7 # due to rate limit on github, we can only have 10 requests per minute for Code Search API - check using   gh api rate_limit
 done
